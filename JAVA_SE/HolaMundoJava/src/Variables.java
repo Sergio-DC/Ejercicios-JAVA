03b55ae37ad84685099ff159f753e963ae7a99ad
Sergio-DC <sgdc377@gmail.com>
2021-07-08 17:44:49 -0500

    Java 1 course finished

---
+++ JAVA_SE/HolaMundoJava/src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        int speed;
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 2000;
+        String employeeName = "Sergio";
+        System.out.println(employeeName);
+
+    }
+}
