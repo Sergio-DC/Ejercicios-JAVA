3747bf726430ee9e700aa0cbd86b2e2264992be3
Sergio-DC <sgdc377@gmail.com>
2021-07-09 09:04:35 -0500

    static elements & UI menu added

---
+++ JAVA_OOP/MyMedicalAppointments/src/Doctor.java
@@ -0,0 +1,25 @@
+import javax.print.Doc;
+
+public class Doctor {
+    //Atributos
+    static int id = 0;
+    String name;
+    String speciality;
+
+    Doctor(){
+        System.out.println("Construyendo el Objeto Doctor");
+        id++;
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del Doctor asignado es: " + name);
+    }
+
+    public void showName(){
+        System.out.println(name);
+    }
+    public void showId() {
+        System.out.println("ID: " + id);
+    }
+
+}
