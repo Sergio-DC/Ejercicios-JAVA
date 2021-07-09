fec184ae1488615fb7d05f91568839ba5b208dc3
Sergio-DC <sgdc377@gmail.com>
2021-07-09 09:31:48 -0500

    Challenge

---
+++ JAVA_OOP/MyMedicalAppointments/src/Patient.java
@@ -0,0 +1,16 @@
+public class Patient {
+        public String name;
+        public String email;
+        public String address;
+        public String phoneNumber;
+        public String birthday;
+        public double weight;
+        public double height;
+        public String blood;
+
+        Patient(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+}
+
