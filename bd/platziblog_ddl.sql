065b550faebe7c336978d1d2ffc32e4614b854ee
Sergio-DC <sgdc377@gmail.com>
2021-07-14 10:29:54 -0500

    Independent tables Platziblog

---
+++ bd/platziblog_ddl.sql
@@ -0,0 +1,22 @@
+CREATE TABLE categorias (
+	id INTEGER NOT NULL,
+	categoria VARCHAR(30) NOT NULL,
+	PRIMARY KEY(ID)
+);
+
+CREATE TABLE etiquetas (
+	id INTEGER NOT NULL PRIMARY KEY,
+	nombre_etiqueta VARCHAR(30)
+);
+
+CREATE TABLE usuarios (
+	id INTEGER NOT NULL PRIMARY KEY,
+	login VARCHAR(30) NOT NULL,
+	password VARCHAR(32) NOT NULL,
+	nickname VARCHAR(40) NOT NULL,
+	email VARCHAR(40) NOT NULL UNIQUE
+);
+
+/*Modify the user id*/
+ALTER TABLE usuarios MODIFY id INT NOT NULL AUTO_INCREMENT;
+
