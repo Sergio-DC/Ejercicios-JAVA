b3662182d88ea6a07c236590140a0eab62e58d05
Sergio-DC <sgdc377@gmail.com>
2021-07-15 18:11:20 -0500

    Terminal commands

---
+++ TERMINAL/comandos.sh
@@ -0,0 +1,14 @@
+sudo apt install <package-name> # Install a package
+mkdir <dir-name> # Create a new directory
+touch <file-name> # Create a new file
+cp <source-file> <destination-file> # Duplicate a the src file to dest file
+mv <source-file> <destinatio-file> # Move or rename a file
+rm <file-name> # Delete a file
+alias rm = 'rm -i' # Give an alias name to the command between in single quotes
+ls # List the files of the current dir or the stated dir
+man <command-name> # Show a detailed explanation about what a command does
+--help # Use this flag after a commando and a brief help will be shown
+head <file-name> # show the first 10 lines of the file
+tail <file-name> # show the last 10 lines of the file
+less <file-name> # the file will be displaye in interactive mode
+xdg-open <file-name> # Open a file with a installed app
