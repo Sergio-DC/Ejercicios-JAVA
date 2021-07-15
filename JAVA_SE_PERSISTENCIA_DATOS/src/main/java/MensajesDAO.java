b7d3d9ac913c26c7d517b7caab05c6a5dd6ba455
Sergio-DC <sgdc377@gmail.com>
2021-07-15 17:10:11 -0500

    Data insertion

--- JAVA_SE_PERSISTENCIA_DATOS/src/main/java/MensajesDAO.java
+++ JAVA_SE_PERSISTENCIA_DATOS/src/main/java/MensajesDAO.java
@@ -1,6 +1,19 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class MensajesDAO {
     public static void crearMensaje(Mensajes mensaje) {
-
+        Conexion dbConnect = new Conexion();
+        try(Connection connection = dbConnect.getConnection()) {
+            String sql = "INSERT INTO mensaje(mensaje, autor_mensaje) VALUES (?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, mensaje.getMensaje());
+            preparedStatement.setString(2, mensaje.getAutorMensaje());
+            preparedStatement.executeUpdate();
+            System.out.println("El mensaje fue creado");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     public static void leerMensajesDB() {
