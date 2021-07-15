447de6b48f930be481754e0115aa4cb2d01d35a9
Sergio-DC <sgdc377@gmail.com>
2021-07-15 16:12:37 -0500

    Application logic

--- JAVA_SE_PERSISTENCIA_DATOS/src/main/java/Inicio.java
+++ JAVA_SE_PERSISTENCIA_DATOS/src/main/java/Inicio.java
@@ -1,11 +1,37 @@
 import java.sql.Connection;
+import java.util.Scanner;
 
 public class Inicio {
     public static void main(String[] args) {
-        try(Connection connection = new Conexion().getConnection()){
-            System.out.println("Entre");
-        } catch (Exception e) {
-            System.out.println("Error al intentar conectar: " + e);
-        }
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0;
+        do {
+            System.out.println("App de Mensajes");
+            System.out.println("1. Crear mensaje");
+            System.out.println("2. Listar mensaje");
+            System.out.println("3. Editar mensaje");
+            System.out.println("4. Eliminar mensaje");
+            System.out.println("5. Salir");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    MensajesService.crearMensaje();
+                    break;
+                case 2:
+                    MensajesService.listarMensajes();
+                    break;
+                case 3:
+                    MensajesService.editarMensaje();
+                    break;
+                case 4:
+                    MensajesService.borrarMensaje();
+                    break;
+                default:
+                    break;
+            }
+        } while (opcion != 5);
+
+
     }
 }
---
+++ JAVA_SE_PERSISTENCIA_DATOS/src/main/java/MensajesDAO.java
@@ -0,0 +1,17 @@
+public class MensajesDAO {
+    public static void crearMensaje(Mensajes mensaje) {
+
+    }
+
+    public static void leerMensajesDB() {
+
+    }
+
+    public static void borrarMensajesDB(int idMensaje) {
+
+    }
+
+    public static void actualizarMensajeDB(Mensajes mensaje) {
+
+    }
+}
