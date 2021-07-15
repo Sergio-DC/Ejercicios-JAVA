0df079050a93f5823490a250d3d9221b033b56dc
Sergio-DC <sgdc377@gmail.com>
2021-07-15 17:27:57 -0500

    Update data

--- JAVA_SE_PERSISTENCIA_DATOS/src/main/java/MensajesService.java
+++ JAVA_SE_PERSISTENCIA_DATOS/src/main/java/MensajesService.java
@@ -26,6 +26,15 @@ public class MensajesService {
         MensajesDAO.borrarMensajesDB(id_mensaje);
     }
     public static void editarMensaje() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("escribe tu nuevo mensaje");
+        String mensaje = sc.nextLine();
 
+        System.out.println("indica el ID del mensaje a editar");
+        int id_mensaje= sc.nextInt();
+        Mensajes actualizacion = new Mensajes();
+        actualizacion.setIdMensaje(id_mensaje);
+        actualizacion.setMensaje(mensaje);
+        MensajesDAO.actualizarMensajeDB(actualizacion);
     }
 }
