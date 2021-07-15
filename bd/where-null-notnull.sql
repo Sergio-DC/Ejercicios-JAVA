99c8a7a46e78a273372f48fdc763d31edcf3b0ea
Sergio-DC <sgdc377@gmail.com>
2021-07-15 08:35:26 -0500

    WHERE null, not null

---
+++ bd/where-null-notnull.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM posts
+WHERE usuario_id IS NULL;
+
+SELECT *
+FROM posts
+WHERE usuario_id IS NOT NULL;
+
+SELECT *
+FROM posts
+WHERE usuario_id IS NOT NULL
+  AND estatus = 'activo'
+  AND id < 50
+  AND categoria_id = 2
+  AND YEAR(fecha_publicacion) = '2025';
